175c5d1effda25f13ecdb0a366501aa63d66bda6
Pedro Oliveira <pedro.oliveira@gpnet.com.br>
2022-08-23 16:05:36 -0300

    Adcionando arquivos padronizados

--- biblioteca/src/main/java/br/com/gep/biblioteca/repositories/AutorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.gep.biblioteca.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.gep.biblioteca.entities.AutorEntity;
-
-public interface AutorRepository extends JpaRepository<AutorEntity, Long>{
-
-}
